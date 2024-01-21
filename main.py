25863bbeb86b613faff1e64864b127ce72e6741b
Anth <50714637+notanth@users.noreply.github.com>
2024-01-20 19:19:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import st-gsheets-connection 
+#import st-gsheets-connection 
 
 st.text('taking sheets and streamlit frames for a spin')
